5a7f489c36bdd9df829e1a0c170d67136270d6e9
Seongsu Park <56729421+seongs1024@users.noreply.github.com>
2024-01-25 12:46:18 +0900

    shared-state concurrency with thread-safe

--- src/market.rs
+++ src/market.rs
@@ -1,6 +1,119 @@
-use crate::data_loader::import_parquet;
-
 use polars::prelude::*;
+use std::sync::{Arc, Mutex};
+use tokio::{
+    sync::broadcast::{self, error::RecvError, Receiver, Sender},
+    time::{sleep, Duration},
+};
+
+// struct MarketData {
+//     idx: usize,
+//     time: i64,
+//     code: String,
+//     price: f64,
+// }
+
+type Itcp = (usize, i64, String, f64);
+
+pub struct Market<MarketData> {
+    df: Arc<DataFrame>,
+    tx: Option<Sender<MarketData>>,
+}
+
+impl Market<Itcp> {
+    pub fn new(df: DataFrame, tx: Sender<Itcp>) -> Self {
+        Self {
+            df: Arc::new(df),
+            tx: Some(tx),
+        }
+    }
+
+    pub fn send(&mut self) {
+        let df = self.df.clone();
+        let Some(tx) = self.tx.take() else { todo!() };
+        tokio::spawn(async move {
+            let mut idx = 0;
+            loop {
+                if let Some(tick) = df.get(idx) {
+                    let AnyValue::Int64(time) = tick[0] else {
+                        todo!()
+                    };
+                    let AnyValue::String(code) = tick[1] else {
+                        todo!()
+                    };
+                    let AnyValue::Float64(price) = tick[2] else {
+                        todo!()
+                    };
+
+                    sleep(Duration::from_micros(100)).await;
+                    match tx.send((idx, time, code.to_string(), price)) {
+                        Ok(_) => {}
+                        Err(e) => {
+                            eprintln!("{}", e);
+                            break;
+                        }
+                    };
+                } else {
+                    break;
+                }
+                idx += 1;
+            }
+        });
+    }
+}
+
+pub struct Strategy<MarketData> {
+    name: Arc<String>,
+    rx: Option<Receiver<MarketData>>,
+}
+
+impl Strategy<Itcp> {
+    pub fn new(name: String, rx: Receiver<Itcp>) -> Self {
+        Self {
+            name: Arc::new(name),
+            rx: Some(rx),
+        }
+    }
+
+    pub fn recv(&mut self) {
+        let name = self.name.clone();
+        let Some(mut rx) = self.rx.take() else {
+            todo!()
+        };
+        tokio::spawn(async move {
+            loop {
+                match rx.recv().await {
+                    Ok(market_data) => println!("{}: {:?}", name, market_data),
+                    Err(RecvError::Lagged(behind)) => {
+                        eprintln!("{}: lagged behind {}", name, behind)
+                    }
+                    Err(RecvError::Closed) => break,
+                }
+            }
+        });
+    }
+}
+
+// Market and Strategies Builder
+pub struct MsBuilder;
+
+impl MsBuilder {
+    #[allow(clippy::new_ret_no_self)]
+    pub fn new(df: DataFrame, strategies: usize) -> (Market<Itcp>, Vec<Strategy<Itcp>>) {
+        let (tx, rx) = broadcast::channel(1_000);
+
+        if strategies < 2 {
+            todo!();
+        }
+        let strategies: Vec<_> = (0..(strategies - 1))
+            .map(|_| tx.subscribe())
+            .chain(std::iter::once(rx))
+            .enumerate()
+            .map(|(i, rx)| Strategy::new(format!("{}", i), rx))
+            .collect();
+        let market = Market::new(df, tx);
+        (market, strategies)
+    }
+}
 
 #[cfg(test)]
 mod tests {
