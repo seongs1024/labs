4b4a15116d0e2e0876449ef359a113d7a61a0248
Seongsu Park <56729421+seongs1024@users.noreply.github.com>
2024-01-25 13:59:17 +0900

    move test(validate_ticks) to data_loader.rs

--- src/market.rs
+++ src/market.rs
@@ -114,37 +114,3 @@ impl MsBuilder {
         (market, strategies)
     }
 }
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use std::ops::Sub;
-
-    #[tokio::test]
-    async fn validate_ticks() -> Result<(), Box<dyn std::error::Error>> {
-        let df = import_parquet("data/kospi_tick.parquet")?;
-        println!("data: {}", df);
-
-        let low_price = df
-            .clone()
-            .lazy()
-            .filter(col("trade_price").lt(lit(10)))
-            .collect()?;
-        println!("price lower than 10: {}", low_price);
-
-        let start_end = df
-            .clone()
-            .lazy()
-            .group_by([col("sec_code")])
-            .agg([
-                col("trade_time").first().alias("first_time"),
-                col("trade_time").last().alias("last_time"),
-            ])
-            .with_column(col("last_time").sub(col("first_time")).alias("duration"))
-            .sort("first_time", Default::default())
-            .collect()?;
-        println!("group by sec_code: {}", start_end);
-
-        Ok(())
-    }
-}
